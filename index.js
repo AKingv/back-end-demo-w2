fbd3ee78b1ea905f9ee1d32bdd8c2f6d9e4e9258
Ariella <arieking@cisco.com>
2023-01-06 14:25:42 -0500

    demo

--- index.js
+++ index.js
@@ -15,8 +15,35 @@ const run = async () => {
     console.log("Let's start hashing some passwords!");
 
     /* *************** START DEMO *************** */
-    
-    
+    const hashedPassword = await bcrypt.hash(userJohn.password, SALT_COUNT)
+    const hashedPassword2 = await bcrypt.hash(userJohn2.password, SALT_COUNT)
+    console.log('hashedPassword: ', hashedPassword)
+    console.log('hashedPassword2: ', hashedPassword2)
+
+
+    const isAMatch = await bcrypt.compare(userJohn.password, hashedPassword)
+    if (isAMatch === true) {
+      console.log("passwords match!")
+    } else if (isAMatch === false) {
+      console.log("incorrect password")
+    }
+
+    const hashedPassswordForUser = await bcrypt.hash(userJohn.password, SALT_COUNT)
+    const safeUser = await User.create({
+      username: userJohn.username,
+      password: hashedPasswordForUser
+    })
+    console.log(">>>>>>>>>>> safeUser", safeUser.get({plain:true}))
+
+
+    const unsafeUser = await User.create({
+      username: userJohn2.username,
+      password: userJohn2.password
+    })
+    console.log(">>>>>>>>>>>> unsafeUser", unsafeUser.get({plain:true}))
+
+
+
   } catch (error) {
     console.error(error)
   } finally {
